3e6980f1c0182e245ed587dc120249663f4d7ec0
CMA <87302380@qq.com>
2023-06-07 00:42:11 +0800

    fixup

--- api/public.py
+++ api/public.py
@@ -1,12 +1,7 @@
 import json
 import os
 
-from rdflib import Graph, Literal, URIRef, BNode
-from rdflib.term import Identifier
-from rdflib.collection import Collection
-from rdflib.namespace import RDF, RDFS, SKOS, XSD, OWL
-import rdflib.plugins.sparql.update
-import owlrl
+from rdflib import Graph
 from rdflib.plugins.sparql import prepareQuery
 
 KnowNow_URL = "https://git.tu-berlin.de/felipebaca/know-now/-/raw/main/KnowNow_V8_ttl.owl"
@@ -18,3 +13,18 @@ def query_sparql(sparql: str) :
     headers = [str(var) for var in results.vars]
     rows = [[str(val) for val in row] for row in results]
     return json.dumps([dict(zip(headers, row)) for row in rows])
+
+def upload_file_to_server(save_to, file):
+
+    status = 'No file uploaded'
+    try:
+        save_directory = os.path.join("/home/knownow/KnowNow/", save_to)
+        file_path = os.path.join(save_directory, file.name)
+        with open(file_path, 'wb') as f:
+            for chunk in file.chunks():
+                f.write(chunk)
+        status = "file uploaded success"
+    except:
+        status = "file uploaded faild"
+
+    return status
--- api/views.py
+++ api/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from api.public import upload_file_to_server, query_sparql
 import json
-import os
 
 def index(request):
 
@@ -11,21 +10,10 @@ def index(request):
 
 @csrf_exempt
 def upload(request):
-    status = 'No file uploaded'
-
     if request.method == 'POST' and request.FILES.get('file'):
         path = request.POST['path']
         file = request.FILES['file']
-
-        try:
-            save_directory = os.path.join("/home/knownow/KnowNow/", path)
-            file_path = os.path.join(save_directory, file.name)
-            with open(file_path, 'wb') as f:
-                for chunk in file.chunks():
-                    f.write(chunk)
-            status = "file uploaded success"
-        except:
-            status = "file uploaded faild"
+        status = upload_file_to_server(path, file)
 
     return HttpResponse(status)
 
